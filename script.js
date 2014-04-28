71227c45f58a91b705d9295a5a11b7b95df620ae
bathrall <bathrall@hotmail.com>
2014-04-28 09:37:18 -0700

    updated js

--- script.js
+++ script.js
@@ -2,9 +2,29 @@ $(document).ready(function() {
     //add by clicking add button
     $("#button").click(function() {
        var toAdd = $("input[name=checkListItem]").val();
-         $(".list").append ('<div class="item">' + toAdd + '</div>');
-        //removes input value
+        
+    toAdd = toAdd.trim()
+    if (0 == toAdd.length) { //prevent adding empty item
+        console.log("no");
+        return;
+    }
+    //removes input value
         $("#object").val("");
+
+        var newItem = $('<div class="item">' + toAdd + '</div>');
+         $(".list").append(newItem);
+
+         //double click to remove
+        $(document).on("dblclick", ".item", function() {
+          $(this).remove ();  
+        }) 
+
+        //click to strike through
+        newItem.click(function() {
+            console.log("running the click bind function on", this);
+            $(this).addClass("crossedOut");
+        })           
+        
     });  
     //add by hitting enter key
  	$('#object').keypress(function(event) {
@@ -14,10 +34,7 @@ $(document).ready(function() {
         }
     });
         
-	//double click to remove
-        $(document).on("dblclick", ".item", function() {
-          $(this).remove ();  
-        })  		    
+	  
 });
 
 
